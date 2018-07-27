042ef4882fbea41a84b793cb4000659534d2a44b
Guillaume Mella <guillaume.mella@univ-grenoble-alpes.fr>
2018-07-27 14:04:48 +0200

    send all obxml or the ones given as argument

--- test/a2p2.sh
+++ test/a2p2.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 # 
 # TODO mke this test using sampy (because this module is for python :) )
+# else we need to install jsamp first 
 
-FILE=aspro-sample.obxml
-
-CURRENT=`pwd`
-
-java -classpath ${CURRENT}/../../target/runtime-libs/jsamp-1.3.5.jar org.astrogrid.samp.test.MessageSender -mtype "ob.load.data" -param "url" "${CURRENT}/$FILE" > /tmp/sendtoVOT.log
-
-if [ $? != 0 ]; then
-    echo "Cannot send to ESO's p2 samp hub, sorry."
+if [ $# -ge 1 ]
+then
+  FILES=$*
+else
+  FILES=*.obxml
 fi
+
+for FILE in $FILES 
+do
+  if ! java -classpath jsamp-1.3.5.jar:${HOME}/.m2/repository/org/astrogrid/jsamp/1.3.5/jsamp-1.3.5.jar:${PWD}/../../target/runtime-libs/jsamp-1.3.5.jar org.astrogrid.samp.test.MessageSender -mtype "ob.load.data" -param "url" "${PWD}/$FILE" > /tmp/sendtoVOT.log
+  then
+    echo "Cannot send to A2P2 through samp hub, sorry."
+  fi
+done
+
 
