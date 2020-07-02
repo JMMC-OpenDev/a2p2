c51aed409557c6e8a21c2d0351005e7f8f8b6ef5
mellag <guillaume.mella@univ-grenoble-alpes.fr>
2020-07-02 16:37:01 +0200

    bump version for new pypi package

--- a2p2/version.py
+++ a2p2/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.11"
+__version__ = "0.2.12"
 
 __release_notes__ = {
     # "0.1.6": {
@@ -21,6 +21,21 @@ __release_notes__ = {
     #
     #        ],
     #    },
+    "0.2.12": {
+        "STATUS": [
+        ],
+        "A2P2": [
+            "Fix import in main console script"
+        ],
+        "VLTI": [
+        ],
+        "CHARA": [
+        ],
+        "TODO-SCIENCE": [
+        ],
+        "TODO-DEV": [
+        ],
+    },
     "0.2.11": {
         "STATUS": [
         ],
