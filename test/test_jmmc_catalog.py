522db5776a4009d2cd2e7f52cf6ffd243ae9dfac
mellag <guillaume.mella@univ-grenoble-alpes.fr>
2021-09-03 08:36:46 +0200

    first basic tests

--- test/test_jmmc_catalog.py
+++ test/test_jmmc_catalog.py
@@ -7,15 +7,65 @@ import pytest
 
 from a2p2.jmmc import Catalog
 
+oidbValidId = 38681
+
+
+class CustomCatalog(Catalog):
+    def __init__(self, catalogName):
+        Catalog.__init__(self, catalogName,
+                         url="http://localhost:8080/exist/restxq/catalogs")
+
+
 def test_same_id():
-    has_same_id("oidb", 38681)
+    has_same_id("oidb", oidbValidId)
+
+
 def test_same_id2():
-    has_same_id("oidb", 38682)
+    has_same_id("oidb", oidbValidId)
 
 
 def has_same_id(catname, id):
-    c = Catalog(catname)
+    c = CustomCatalog(catname)
     r = c.getRow(id)
     assert r["id"] == id
-    print ("We are ok with catalog %s id %s"%(catname, id))
+    print("We are ok with catalog %s id %s" % (catname, id))
     return
+
+def test_protected():
+    spica = CustomCatalog("spica")
+    try:
+        spica.getRow(1)
+        spicaIsProtected = False
+    except:
+        spicaIsProtected = True
+
+    assert spicaIsProtected
+
+
+def test_public():
+    oidb = CustomCatalog("oidb")
+    try:
+        oidb.getRow(oidbValidId)
+        oidbIsPublic = True
+    except:
+        oidbIsPublic = False
+    assert oidbIsPublic
+
+def test_simple_update():
+    c = CustomCatalog("spica")
+    v=2
+    c.updateRow(1, '{"target_priority_pi":%d}'%v)
+    priority = c.getRow(1)["target_priority_pi"]
+    assert priority==v
+
+
+def test_duplicated_col_update():
+    c = CustomCatalog("spica")
+
+    c.updateRow(1, '{"target_priority_pi":1, "target_priority_pi",2}')
+    priority = c.getRow(1)["target_priority_pi"]
+    assert priority==2
+
+    c.updateRow(1, '{"target_priority_pi":1, "TARGET_PRIORITY_PI",2}')
+    priority = c.getRow(1)["target_priority_pi"]
+    assert priority==2
